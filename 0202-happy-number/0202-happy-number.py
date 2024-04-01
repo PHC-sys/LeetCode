92f6053cd681f889c07da269665db4523dc5081d
PHC-sys <94788938+PHC-sys@users.noreply.github.com>
2024-04-01 13:36:03 +0900

    Time: 43 ms (18.69%), Space: 16.5 MB (38.78%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1 :
+            return True
+        if n < 6:
+            return False
+
+        sumx = 0;
+        for i in str(n):
+            sumx = sumx + int(i)*int(i);
+
+        return self.isHappy(sumx)
