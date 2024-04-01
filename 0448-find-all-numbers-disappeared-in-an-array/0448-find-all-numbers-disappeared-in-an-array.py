f0247356a8787615bd6315cc740709061d47252a
PHC-sys <94788938+PHC-sys@users.noreply.github.com>
2024-04-01 13:52:00 +0900

    Time: 276 ms (29.04%), Space: 24.9 MB (84.60%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # For each number i in nums,
+        # we mark the number that i points as negative.
+        # Then we filter the list, get all the indexes
+        # who points to a positive number
+        for i in range(0,len(nums)):
+            index = abs(nums[i]) - 1
+            nums[index] = - abs(nums[index])
+        return [i + 1 for i in range(len(nums)) if nums[i] > 0]
